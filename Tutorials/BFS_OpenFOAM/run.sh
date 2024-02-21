ab33e8503ca710d881b2400f5029a8526ae90fb3
nipinl <nipinl@gmail.com>
2024-02-21 13:53:55 -0500

    modified to work with both ESI and Foundation version

--- Tutorials/BFS_OpenFOAM/run.sh
+++ Tutorials/BFS_OpenFOAM/run.sh
@@ -1,6 +1,6 @@
 mesh=bfs_300k
 case=bfs_DDES_300k
-gmsh mesh/$mesh.geo -3 -o mesh/$mesh.msh
+gmsh mesh/$mesh.geo -3 -o mesh/$mesh.msh -format msh2
 gmshToFoam mesh/$mesh.msh -case $case
 cp $case/constant/polyMesh/boundary $case/constant/polyMesh/boundary.bkp
 cp mesh/boundary $case/constant/polyMesh/

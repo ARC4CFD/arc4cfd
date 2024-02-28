13bbe47ecbc126ecde8a3f4a24fed72f4d7b6094
nipinl <nipinl@gmail.com>
2024-02-28 16:47:33 -0500

    scaling tests added

---
+++ Tutorials/scalingTests/strongScalingTest/case/jobscript.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --ntasks=8
+#SBATCH --mem-per-cpu=3g
+#SBATCH --time=0-00:30
+#SBATCH --job-name="bfs8"          # job name
+#SBATCH --output=bfs8%j.txt
+#SBATCH --mail-type=FAIL
+
+
+cd $SLURM_SUBMIT_DIR
+module load StdEnv/2023  gcc/12.3  openmpi/4.1.5 openfoam/v2306 gmsh/4.12.2
+. $WM_PROJECT_DIR/bin/tools/RunFunctions
+
+cp -r 0.orig 0
+decomposePar -force > log.decomposePar
+mpirun pimpleFoam -parallel > log.pimpleFoam
+reconstructPar > log.reconstructPar
+rm -rf processor*
+touch completed

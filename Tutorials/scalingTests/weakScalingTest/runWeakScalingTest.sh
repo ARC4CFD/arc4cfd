42b7973f9f351631db5cd380888f100e9bd5e494
nipinl <nipinl@gmail.com>
2024-03-04 12:57:45 -0500

    strong scaling result for niagara and graham added

--- Tutorials/scalingTests/weakScalingTest/runWeakScalingTest.sh
+++ Tutorials/scalingTests/weakScalingTest/runWeakScalingTest.sh
@@ -8,16 +8,18 @@ module load StdEnv/2023  gcc/12.3  openmpi/4.1.5 openfoam/v2306 gmsh/4.12.2
 #-------------------------------
 echo "STEP1: preparing the base case"
 rm -rf bfs[0-9]*	# remove directories created during the previous scaling test
+rm -rf mesh/bfs[1-9]*	#remove old gmsh and mesh files
 
-for i in 1 2 3 4 5 6 7 8; do
+for i in 1 2 4 6 8 12 16 20 24 28 32; do
+#for i in 1 2 3 4 5 6 7 8; do
 #for i in 1 2 4 8; do #less no. of cases for quick testing
     case=bfs$i
     echo "Prepare case $case..."
     cp -r case $case
     cp mesh/bfs.geo mesh/$case.geo
-    nz=$(echo "scale=1;4 * $i"|bc)
+    nz=$(echo "scale=1;2 * $i"|bc)
     echo "no of divisions in Z direction= $nz"
-    sed -i "s/Nz = 4;/Nz = $nz;/" mesh/$case.geo	#increase no. of division in z direction to keep same load per proc
+    sed -i "s/Nz = 2;/Nz = $nz;/" mesh/$case.geo	#increase no. of division in z direction to keep same load per proc
     cd $case
     sed -i "s/numberOfSubdomains.*/numberOfSubdomains ${i};/" system/decomposeParDict	#using right no. of procs
     sed -i "s/1 1 .*);/1 1 ${i});/" system/decomposeParDict	#using right no. of procs
@@ -51,8 +53,8 @@ echo "# cores   Wall time (s):"
 echo "------------------------"
 echo "# cores   Wall time (s):"> WSTResults.dat
 echo "------------------------">> WSTResults.dat
-#for i in 1 2 4 6 8 12 16 20 24 28 32; do
-for i in 1 2 3 4 5 6 7 8; do
+for i in 1 2 4 6 8 12 16 20 24 28 32; do
+#for i in 1 2 3 4 5 6 7 8; do
 #for i in 1 2 4 8; do
     echo $i `grep Execution bfs${i}/log.pimpleFoam | tail -n 1 | cut -d " " -f 3`
     echo $i `grep Execution bfs${i}/log.pimpleFoam | tail -n 1 | cut -d " " -f 3`>> WSTResults.dat
